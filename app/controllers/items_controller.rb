a51c179a0b99b702e81ea3c86dd277e1ee220766
hisashi0315 <g17h.tagai@ikubun.com>
2019-05-02 15:32:33 +0900

    登録、更新機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,38 @@
 class ItemsController < ApplicationController
 
-   def show
-   # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
-   # 取得したデータを@itemというインスタンス変数にセットする
-   @item = Item.find(params[:id])
-   end
-   def index
+  def show
+  @item = Item.find(params[:id])
+  end
+
+  def index
     @Items = Item.all
-   end
+  end
+
+  def new
+    @item = Item.new 
+  end
+
+
+def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+  def edit
+  @item = Item.find (params[:id])
+  end
+
+  def update
+  @item =Item.find(params[:id])
+  @item.update(item_params)
+  redirect_to @item
+  end
+
+  private
+
+  def item_params
+  params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
+  
+
 end
